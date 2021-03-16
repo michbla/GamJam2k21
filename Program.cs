3250d2cba4a1d915b4953f393360c803f3b0ea71
Diegothic <diegothic24@gmail.com>
2021-03-16 14:42:16 +0100

    a

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Mathematics;
 
 namespace GamJam2k21
 {
@@ -7,6 +9,19 @@ namespace GamJam2k21
         static void Main(string[] args)
         {
             Console.WriteLine("KOPALNIARZE AUUUUU");
+
+            //Ustawienia okna
+            var nativeWindowSettings = new NativeWindowSettings()
+            {
+                Size = new Vector2i(800, 600),
+                Title = "Gam Jam Lato 2K21",
+            };
+            //Ustawienia odswierzania
+            var gameWindowSettings = new GameWindowSettings()
+            {
+                //RenderFrequency = 60,
+                //UpdateFrequency = 60,
+            };
         }
     }
 }

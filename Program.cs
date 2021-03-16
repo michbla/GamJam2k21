a034327adb101e92bce45de04ecc682473da12dc
michbla <michalblaszczynski97@gmail.com>
2021-03-16 13:58:51 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GamJam2k21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("KOPALNIARZE AUUUUU");
+        }
+    }
+}
